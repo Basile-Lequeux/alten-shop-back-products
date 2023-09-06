bb80dae75c39eae9cc6bc95ff135579bbbb9a66f
Basile Lequeux <45491232+Basile-Lequeux@users.noreply.github.com>
2023-09-06 17:11:16 +0200

    add productRoutes

--- back/src/server.js
+++ back/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { db, initDb } = require('./database');
-const productController = require("./controllers/productController");
+const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 const PORT = 3000;
@@ -16,12 +16,7 @@ app.use(bodyParser.json());
 // Enable CORS middleware to allow cross-origin requests
 app.use(cors())
 
-// Routes
-app.post('/products', productController.createProduct);
-app.get('/products', productController.getAllProducts);
-app.get('/products/:id', productController.getProductById);
-app.delete('/products/:id', productController.deleteProduct);
-app.patch('/products/:id', productController.updateProduct);
+app.use(productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
