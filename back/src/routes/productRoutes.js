59dbdb9c35afef8b68284cd8fe1b788e41a30e96
basile <basile-lequeux@hotmail.fr>
2023-09-12 20:20:15 +0200

    add swagger

--- back/src/routes/productRoutes.js
+++ back/src/routes/productRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const productController = require("../controllers/productController");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.post('/products', productController.createProduct);
-
 router.get('/products', productController.getAllProducts);
 router.get('/products/:id', productController.getProductById);
 router.delete('/products/:id', productController.deleteProduct);
