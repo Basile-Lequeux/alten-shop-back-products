6195aa6bed1ada32e5577247a5596aadcf62515f
basile <basile-lequeux@hotmail.fr>
2023-09-12 20:41:44 +0200

    add validateProductBody

--- back/src/routes/productRoutes.js
+++ back/src/routes/productRoutes.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const {validateProductBody} = require("../services/productService");
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-router.post('/products', productController.createProduct);
+router.post('/products', validateProductBody, productController.createProduct);
 router.get('/products', productController.getAllProducts);
 router.get('/products/:id', productController.getProductById);
 router.delete('/products/:id', productController.deleteProduct);
-router.patch('/products/:id', productController.updateProduct);
+router.patch('/products/:id', validateProductBody, productController.updateProduct);
 
 module.exports = router;
--- back/src/services/productService.js
+++ back/src/services/productService.js
@@ -9,3 +9,32 @@ exports.getAllProducts = (callback) => {
         callback(null, rows);
     });
 };
+
+exports.validateProductBody = (req, res, next) => {
+    const product = req.body;
+
+    if (!product) {
+        return res.status(400).send({error: 'Product data is missing.'});
+    }
+
+    const expectedProperties = {
+        code: 'string',
+        name: 'string',
+        description: 'string',
+        price: 'number',
+        quantity: 'number',
+        inventoryStatus: 'string',
+        category: 'string',
+        image: 'string',
+        rating: 'number'
+    };
+
+    for (const [key, type] of Object.entries(expectedProperties)) {
+        if (typeof product[key] !== type) {
+            return res.status(400).send({error: `Expected ${key} to be a ${type}.`});
+        }
+    }
+
+
+    next();
+}
